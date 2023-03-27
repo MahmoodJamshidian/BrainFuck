cf9917add34dd0ddccdfd9e090118146cd25cd30
Mahmood <61678782+MahmoodJamshidian@users.noreply.github.com>
2023-03-27 09:22:08 +0330

    extra line removed

--- brainfuck.cpp
+++ brainfuck.cpp
@@ -152,7 +152,6 @@ int main(int argc, char const *argv[])
     file.close();
     for(int _ = 0; _ < INITIAL_REGISTRY_ARGS; _++)
         reg.nreg.push_back(0);
-    std::cout << code << std::endl;
     Structure main_struct((const char *)code);
     Environment program({{0, strlen(code), &main_struct, false, main_struct.tree}});
     if (!is_build)
